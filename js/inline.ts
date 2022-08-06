9d98f944f1351c90682523d071918a0e296e261b
Ahmed Thahir <ahmedthahir2002@gmail.com>
2022-08-06 20:23:22 +0400

    r

--- js/inline.ts
+++ js/inline.ts
@@ -1,28 +1,44 @@
-const scripts = ["lazyLoad", "nav"]; // , "gallery"
-//- const styles = ["nav"];
+// const styles = ["nav"];
+const scripts = [
+	"nav",
+	"lazyLoad"
+]; // order matters
 
-function loadScript(name){
-	const script = document.createElement("script");
+function loadScript(name:string){
+	const script = <HTMLScriptElement> document.createElement("script");
 	script.type = 'text/javascript';
 	script.src = '../js/' + name + '.js';
 	document.body.appendChild(script);
 }
 
-function loadStyle(name){
-	const style = document.createElement('link');
+function loadStyle(name:string){
+	const style = <HTMLLinkElement> document.createElement('link');
 	style.rel="stylesheet";
 	style.type = 'text/css';
 	style.href = '../css/' + name + '.css';
 	document.head.append(style);
 }
 
+function loadFavicon(){
+	const favicon = <HTMLLinkElement> document.createElement('link');
+	favicon.rel="icon";
+	favicon.type = 'image/x-icon';
+	favicon.href = "../assets/logo/favicon.png";
+	document.head.append(favicon);
+}
+
+
 // purposely put this eager code here, so that the animation is a little delayed
 window.addEventListener("load", function(){
+	
 	const eager = document.querySelectorAll(".eager")
 	if(eager!=null)
 		eager.forEach(element => {
 			element.classList.add("loaded")
-		});
+		})
+
+	// styles.forEach(loadStyle)
 	scripts.forEach(loadScript)
-	//- styles.forEach(loadStyle)
+
+	loadFavicon();
 });
